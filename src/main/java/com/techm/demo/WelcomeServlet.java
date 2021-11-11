77f96655ad91847b630a20f4f182d9aa00286e26
kalyankumar-do-it <78216127+kalyankumar-do-it@users.noreply.github.com>
2021-11-12 01:41:25 +0530

    Initial commit

---
+++ src/main/java/com/techm/demo/WelcomeServlet.java
@@ -0,0 +1,22 @@
+package com.techm.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet
+{
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		String name = req.getParameter("name");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("Welocme to Tech Mahindra :"+name);
+	}
+
+}
